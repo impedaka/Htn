f6d85a6fc4b8cf2eb19d84b90973e6ce675a25a7
Mustafa Abdulrahman <mus2003.abdul@gmail.com>
2022-09-18 02:56:34 -0400

    Changed title to Soul Talk

--- client/src/Homepage.js
+++ client/src/Homepage.js
@@ -1,26 +1,30 @@
 import {Button, Link} from "@chakra-ui/react";
 import React from "react";
+import image from './Assets/img.png';
+
 
 const Diaries = () => {
     return (
         <div>
+            <img src={image} alt="BigCo Inc. logo"/>
             <h1 style={{
                 position: "absolute",
                 width: "465px",
                 height: "96px",
-                left: "528px",
+                left: "678px",
                 top: "397px",
                 fontFamily: 'Inter',
                 fontStyle: "normal",
                 fontWeight: 700,
                 fontSize: "60px",
-                lineHeight: "160%"}}>Hello, I'm Soul</h1>
+                lineHeight: "160%"
+            }}>Hello, I'm Soul</h1>
 
             <p style={{
                 position: "absolute",
                 width: "670px",
                 height: "96px",
-                left: "528px",
+                left: "678px",
                 top: "488px",
                 fontFamily: 'Inter',
                 fontStyle: "normal",
