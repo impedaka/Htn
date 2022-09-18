8327aa871dbc9bf7e5fd6561d4e912a6988ccd43
Mustafa Abdulrahman <mus2003.abdul@gmail.com>
2022-09-18 02:23:36 -0400

    Code for hackathon

---
+++ client/src/Homepage.js
@@ -0,0 +1,36 @@
+import {Button, Link} from "@chakra-ui/react";
+import React from "react";
+
+const Diaries = () => {
+    return (
+        <div>
+            <h1 style={{
+                position: "absolute",
+                width: "465px",
+                height: "96px",
+                left: "528px",
+                top: "397px",
+                fontFamily: 'Inter',
+                fontStyle: "normal",
+                fontWeight: 700,
+                fontSize: "60px",
+                lineHeight: "160%"}}>Hello, I'm Soul</h1>
+
+            <p style={{
+                position: "absolute",
+                width: "670px",
+                height: "96px",
+                left: "528px",
+                top: "488px",
+                fontFamily: 'Inter',
+                fontStyle: "normal",
+                fontWeight: 700,
+                fontSize: "60px",
+                lineHeight: "160%"
+            }}><em>I'm so glad you're here</em></p>
+
+        </div>
+    );
+};
+
+export default Diaries;
